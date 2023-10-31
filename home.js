38b0ad2c861db550e02bfe6ac06f63d514c4868e
PranavRao18 <prnvraosrs@gmail.com>
2023-10-31 12:13:18 +0530

    final responsive homepage

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 let earthquakeData;
+let filteredData;
 
 const twoDaysAgo = new Date();
 twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
@@ -6,6 +7,7 @@ twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
 const formattedDate = twoDaysAgo.toISOString().split('T')[0]; // Format date for API call
 
 const earthquakeURL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${formattedDate}&endtime=now&minmagnitude=4.5`;
+const othersURL = 'https://eonet.gsfc.nasa.gov/api/v2.1/events';
 
 fetch(earthquakeURL)
     .then(response => response.json())
@@ -25,7 +27,7 @@ fetch(earthquakeURL)
             };
         });
 
-        initializeMapView();
+        eonet();
 
         if (earthquakeData && earthquakeData.length > 0) {
             earthquakeData.forEach(event => {
@@ -39,37 +41,41 @@ fetch(earthquakeURL)
         console.error('Error fetching earthquake data', error);
     });
 
-const othersURL = 'data.json';
+// const othersURL = 'data.json';
 
-let filteredData;
-fetch(othersURL)
-    .then(response => response.json())
-    .then(data => {
-        const eventData = data.events;
-        const eventsIn2023 = eventData.filter(event => {
-            const date = new Date(event.geometries[0].date);
-            return date.getFullYear() === 2023;
+function eonet() {
+
+    fetch(othersURL)
+        .then(response => response.json())
+        .then(data => {
+            const eventData = data.events;
+            const eventsIn2023 = eventData.filter(event => {
+                const date = new Date(event.geometries[0].date);
+                return date.getFullYear() === 2023;
+            });
+            filteredData = eventsIn2023.map(event => {
+                return {
+                    id: event.id,
+                    title: event.title,
+                    description: event.description,
+                    category: event.categories[0].title,
+                    source: event.sources[0].url,
+                    coordinates: [event.geometries[0].coordinates[1], event.geometries[0].coordinates[0]]
+                };
+            });
+
+            initializeMapView();
+
+            // Perform operations using the coordinates
+            console.log(coordinates);
+
+            // Or use filteredData directly if needed
+            console.log(filteredData);
+        })
+        .catch(error => {
+            console.error('Error fetching the eonet', error);
         });
-        filteredData = eventsIn2023.map(event => {
-            return {
-                id: event.id,
-                title: event.title,
-                description: event.description,
-                category: event.categories[0].title,
-                source: event.sources[0].url,
-                coordinates: [event.geometries[0].coordinates[1], event.geometries[0].coordinates[0]]
-            };
-        });
-
-        // Perform operations using the coordinates
-        console.log(coordinates);
-
-        // Or use filteredData directly if needed
-        console.log(filteredData);
-    })
-    .catch(error => {
-        console.error('Error fetching the JSON file', error);
-    });
+}
 
 const map = L.map('map', {
     maxZoom: 18,
@@ -77,29 +83,13 @@ const map = L.map('map', {
 });
 
 function setInitialView() {
-    if (window.innerWidth <= 576) {
-
-        map.setView([30, 10], 3);
-        const boundsSmallScreen = L.latLngBounds(
-            L.latLng(-90, -60),
-            L.latLng(90, 60)
-        );
-        map.setMaxBounds(boundsSmallScreen);
-        map.options.minZoom = 3;
-        map.on('zoomend', function () {
-            if (map.getZoom() < 3) {
-                map.setZoom(3);
-            }
-        });
-    } else {
-        map.setView([30, 10], 3);
-        const boundsLargeScreen = L.latLngBounds(
-            L.latLng(-90, -180),
-            L.latLng(90, 180)
-        );
-        map.setMaxBounds(boundsLargeScreen);
-        map.options.minZoom = 3;
-    }
+    map.setView([30, 10], 2);
+    const boundsLargeScreen = L.latLngBounds(
+        L.latLng(-90, -180),
+        L.latLng(90, 180)
+    );
+    map.setMaxBounds(boundsLargeScreen);
+    map.options.minZoom = 2;
 }
 
 
@@ -124,7 +114,7 @@ function initializeMapView() {
             });
 
             marker.on('click', function () {
-                const url = "https://www.google.com/search?q=" + encodeURIComponent(dataOne.title);
+                const url = "https://www.google.com/search?q=" + encodeURIComponent("Earthquake " + dataOne.title);
                 window.open(url, '_blank');
             });
 
@@ -136,7 +126,7 @@ function initializeMapView() {
 
     filteredData.forEach(dataOne => {
         const marker = L.marker(dataOne.coordinates).addTo(map);
-        marker.bindPopup(dataOne.title, { closeOnClick: false, autoClose: false });
+        marker.bindPopup(dataOne.title, '\n' + dataOne.description, { closeOnClick: false, autoClose: false });
 
         marker.on('mouseover', function () {
             marker.openPopup();
@@ -151,16 +141,17 @@ function initializeMapView() {
             window.open(url, '_blank');
         });
 
-        console.log(dataOne.category);
+        console.log(dataOne.description);
 
         if (dataOne.category === 'Volcanoes') {
             marker._icon.classList.add('black-marker');
         } else if (dataOne.category === 'Wildfires') {
-            marker._icon.classList.add('orange-marker');
-        } else if (dataOne.category === 'Severe storms') {
             marker._icon.classList.add('green-marker');
+        } else if (dataOne.category === 'Sea and Lake Ice') {
+            marker._icon.classList.add('purple-marker');
+        } else if (dataOne.category === 'Severe Storms') {
+            marker._icon.classList.add('blue-marker');
         } else {
-            // Add a default color if category doesn't match specific ones
             marker._icon.classList.add('default-marker');
         }
     });

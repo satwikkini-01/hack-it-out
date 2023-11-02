63792c9f8b3c2815cebe81ddfd860d1b88e0e554
PranavRao18 <prnvraosrs@gmail.com>
2023-11-02 12:41:27 +0530

    uiless chatbot

--- crisis/static/js/home.js
+++ crisis/static/js/home.js
@@ -160,3 +160,47 @@ window.onload = function () {
 window.onresize = function () {
     initializeMapView();
 };
+
+//Chatbot:
+
+const chatCircle = document.querySelector('.chat-circle');
+const chatBox = document.querySelector('.chat-box');
+const closeButton = document.querySelector('.close-button');
+const chatBoxMessages = document.getElementById('chat-box-messages');
+const userInput = document.getElementById('user-input');
+const sendButton = document.getElementById('send-button');
+
+chatCircle.addEventListener('click', () => {
+  chatBox.style.right = '20px';
+  chatBox.querySelector('.chat-box-header').style.display = 'block';
+  chatBox.querySelector('.chat-box-messages').style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  chatBox.style.right = '-300px';
+});
+
+function sendMessage(message) {
+  const userMessage = document.createElement('div');
+  userMessage.className = 'message user-message';
+  userMessage.innerHTML = message;
+  chatBoxMessages.appendChild(userMessage);
+}
+
+function receiveMessage(message) {
+  const botMessage = document.createElement('div');
+  botMessage.className = 'message bot-message';
+  botMessage.innerHTML = message;
+  chatBoxMessages.appendChild(botMessage);
+}
+
+sendButton.addEventListener('click', () => {
+  const message = userInput.value;
+  if (message.trim() !== '') {
+    sendMessage(message);
+    setTimeout(() => {
+      receiveMessage("This is a bot response.");
+    }, 1000);
+    userInput.value = '';
+  }
+});
